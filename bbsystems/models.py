f6b6a502f74ee758a3540b530ad4fe495da08bd9
harper <harper@harpers-mbp.attlocal.net>
2015-12-24 11:12:25 -0600

    Initial commit

---
+++ bbsystems/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=128)
+    short = models.CharField(max_length=20)
+
+class Game(models.Model):
+    gid = models.CharField(max_length=30)
+    date = models.DateField()
+    home_team = models.ForeignKey(Team, related_name="home_team_game")
+    away_team = models.ForeignKey(Team, related_name="away_team_game")
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
+
+class Atbat(models.Model):
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    inning = models.IntegerField(blank=True, null=True)
+    top_bottom = models.IntegerField(blank=True, null=True)
+    num = models.IntegerField(blank=True, null=True)
+    b = models.IntegerField(blank=True, null=True)
+    s = models.IntegerField(blank=True, null=True)
+    o = models.IntegerField(blank=True, null=True)
+    start_tfs = models.IntegerField(blank=True, null=True)
+    start_tfs_zulu = models.DateTimeField(blank=True, null=True)
+    batter = models.IntegerField(blank=True, null=True)
+    stand = models.CharField(max_length=3, blank=True, null=True)
+    b_height = models.CharField(max_length=10, blank=True, null=True)
+    pitcher = models.IntegerField(blank=True, null=True)
+    p_throws = models.CharField(max_length=3, blank=True, null=True)
+    des = models.CharField(max_length=1028, blank=True, null=True)
+    des_es = models.CharField(max_length=1028, blank=True, null=True)
+    event_num = models.IntegerField(blank=True, null=True)
+    event = models.CharField(max_length=40, blank=True, null=True)
+    event_es = models.CharField(max_length=40, blank=True, null=True)
+    event2 = models.CharField(max_length=40, blank=True, null=True)
+    event2_es = models.CharField(max_length=40, blank=True, null=True)
+    event3 = models.CharField(max_length=40, blank=True, null=True)
+    event3_es = models.CharField(max_length=40, blank=True, null=True)
+    play_guid = models.CharField(max_length=40, blank=True, null=True)
+    home_team_runs = models.IntegerField(blank=True, null=True)
+    away_team_runs = models.IntegerField(blank=True, null=True)
+
+    score = models.CharField(max_length=3, blank=True, null=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
+
+class Pitch(models.Model):
+
+    # Maps reserved words used in external resources to their names in
+    # this model
+    reserved_fixes = {
+        'id': 'external_id',
+        'type': 'pitch_type',
+    }
+
+    atbat = models.ForeignKey(Atbat, on_delete=models.CASCADE)
+
+    des = models.CharField(max_length=1028, blank=True, null=True)
+    des_es = models.CharField(max_length=1028, blank=True, null=True)
+
+    # This is the field that corresponds to "id" in the xml files. It has to be
+    # renamed because "id" is reserved in django
+    external_id = models.IntegerField(blank=True, null=True)
+
+    # This is the field that corresponds to "type" in the xml files.
+    # type is a reserved word in python
+    pitch_type = models.CharField(max_length=3, blank=True, null=True)
+
+    tfs = models.IntegerField(blank=True, null=True)
+    tfs_zulu = models.DateTimeField(blank=True, null=True)
+    x = models.DecimalField(max_digits=7, decimal_places=4, blank=True)
+    y = models.DecimalField(max_digits=7, decimal_places=4, blank=True)
+    event_num = models.IntegerField(blank=True, null=True)
+    sv_id = models.CharField(max_length=40, blank=True, null=True)
+    play_guid = models.CharField(max_length=40, blank=True, null=True)
+    start_speed = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    end_speed = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    sz_top = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    sz_bot = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    pfx_x = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    pfx_z = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    px = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    pz = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    x0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    y0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    z0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    vx0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    vy0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    vz0 = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    ax = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    ay = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    az = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    break_y = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    break_angle = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    break_length = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    type_confidence = models.DecimalField(max_digits=7, decimal_places=4, blank=True, null=True)
+    zone = models.IntegerField(blank=True, null=True)
+    nasty = models.IntegerField(blank=True, null=True)
+    spin_dir = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    spin_rate = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    cc = models.CharField(max_length=1000, blank=True, null=True)
+    mt = models.CharField(max_length=1000, blank=True, null=True)
+
+    on_1b = models.IntegerField(blank=True, null=True)
+    on_2b = models.IntegerField(blank=True, null=True)
+    on_3b = models.IntegerField(blank=True, null=True)
+
+    balls = models.IntegerField()
+    strikes = models.IntegerField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
---
+++ bbsystems/tasks.py
@@ -0,0 +1,172 @@
+import datetime
+import re
+import requests
+import xml.etree.ElementTree as ET
+
+# For profiling example
+#import os
+#from django.core.wsgi import get_wsgi_application
+#os.environ['DJANGO_SETTINGS_MODULE']='bbsystems.settings'
+#application = get_wsgi_application()
+
+from bbsystems.models import Team, Game, Atbat, Pitch
+
+def get_games_for_range(start_date, end_date):
+    """
+    Get a list of all game links in an inclusive date range
+    """
+
+    games_list = []
+    current_date = start_date
+
+    while current_date < end_date and current_date < datetime.date.today():
+        games_list = games_list + get_games_for_day(current_date)
+        current_date = current_date + datetime.timedelta(days=1)
+
+    return games_list
+
+def get_games_for_day(date=datetime.date.today() - datetime.timedelta(days=1)):
+    """
+    Get a list of game links for a certain day
+    """
+
+    url = 'http://gd2.mlb.com/components/game/mlb/year_' + str(date.year) + '/month_'
+    if date.month < 10:
+        url += '0'
+    url += str(date.month) + '/day_'
+    if date.day < 10:
+        url += '0'
+    url += str(date.day)
+    r = requests.get(url)
+    games = re.findall(r'href="(gid_\d{4}_\d{2}_\d{2}_[^_]{6}_[^_]{6}_\d)', r.text)
+    game_links = [url + '/' + x for x in games]
+    return game_links
+
+def process_games(game_links):
+    """
+    Import data from multiple games given their links
+    """
+
+    for game_link in game_links:
+        process_game(game_link)
+
+def process_pitch(pitch_elem, atbat, balls, strikes):
+    """
+    Create a pitch object from an xml node
+    """
+
+    defaults = pitch_elem.attrib
+
+    # had to rename invalid column names in model.
+    for reserved_word in Pitch.reserved_fixes.keys():
+        defaults[Pitch.reserved_fixes[reserved_word]] = defaults[reserved_word]
+        del(defaults[reserved_word])
+
+    defaults['balls'] = balls
+    defaults['strikes'] = strikes
+
+    # Sometimes these have nice guids. Sometimes we have to make due
+    # without them.
+    if 'play_guid' in pitch_elem.attrib and pitch_elem.attrib['play_guid'] != '':
+        pitch,pitch_created = Pitch.objects.get_or_create(
+            atbat = atbat,
+            play_guid = pitch_elem.attrib['play_guid'],
+            defaults = defaults,
+        )
+        if not pitch_created:
+            map(lambda x: setattr(pitch, x, defaults[x]), defaults)
+            pitch.save()
+        return pitch
+    else:
+        pitch,pitch_created = Pitch.objects.get_or_create(
+            atbat = atbat,
+            external_id = defaults['external_id'],
+            defaults = defaults,
+        )
+        if not pitch_created:
+            map(lambda x: setattr(pitch, x, defaults[x]), defaults)
+            pitch.save()
+        return pitch
+
+def process_atbat(atbat_elem, top_bottom, inning, game):
+    """
+    Create an atbat object from an xml node.
+    Call process_pitch for every pitch in that atbat
+    """
+    defaults = atbat_elem.attrib
+    defaults['inning'] = inning
+    defaults['top_bottom'] = top_bottom
+
+    # Sometimes these have nice guids. Sometimes we have to make due
+    # without them
+    if 'play_guid' in atbat_elem.attrib:
+        atbat,ab_created = Atbat.objects.get_or_create(
+            game = game,
+            play_guid = atbat_elem.attrib['play_guid'],
+            defaults = defaults,
+        )
+        if not ab_created:
+            map(lambda x: setattr(atbat, x, defaults[x]), defaults)
+            atbat.save()
+    else:
+        atbat,ab_created = Atbat.objects.get_or_create(
+            game = game,
+            num = atbat_elem.attrib['num'],
+            defaults = defaults,
+        )
+        if not ab_created:
+            map(lambda x: setattr(atbat, x, defaults[x]), defaults)
+            atbat.save()
+
+    # The count isn't actually in the pitch data. We have to keep track.
+    balls = 0
+    strikes = 0
+
+    for pitch_elem in atbat_elem:
+        if pitch_elem.tag == 'pitch':
+            pitch = process_pitch(pitch_elem, atbat, balls, strikes)
+            if pitch.pitch_type == 'B':
+                balls += 1
+            else:
+                strikes += 1
+
+def process_game(game_link):
+        """
+        Create a Game object. Query the mlbam gameday xml files for that game.
+        Call process_atbat for each atbat in that game
+        """
+
+        inning_url = game_link + '/inning/inning_all.xml'
+        inning_r = requests.get(inning_url)
+        if inning_r.status_code != 200:
+            return
+            # raise Exception('Could not find game_link: ' + game_link)
+        root = ET.fromstring(inning_r.text.replace(u'\xed','').encode('utf-8').strip())
+
+        match = re.search(r'(gid_(\d{4})_(\d{2})_(\d{2})_[^_]{6}_[^_]{6}_\d)', game_link)
+        gid = match.group(1)
+        date = datetime.date(int(match.group(2)), int(match.group(3)), int(match.group(4)))
+
+        teams = re.findall(r'_([^_]{3})[^_]{3}',gid)
+
+        away_team = Team.objects.get(short = teams[0])
+        home_team = Team.objects.get(short = teams[1])
+
+        game,created = Game.objects.get_or_create(
+            gid = gid,
+            date = date,
+            home_team = home_team,
+            away_team = away_team,
+        )
+
+        # Innings and top/bottom are tedious and meaningless relationships.
+        # We're just going to pin them in to the at bats.
+        for inning_elem in root:
+            inning = inning_elem.attrib['num']
+            for top_bottom_elem in inning_elem:
+                top_bottom = 1
+                if top_bottom_elem.tag == 'bottom':
+                    top_bottom = 0
+                for atbat_elem in top_bottom_elem:
+                    if atbat_elem.tag == 'atbat':
+                        process_atbat(atbat_elem, top_bottom, inning, game)
